c6b8f6d0bf5be69fa57fd128d05067bb1e492c42
aligolgol <golgol.postbox@gmail.com>
2021-05-28 17:52:03 +0430

    MaximumUniqueSubarray

---
+++ src/test/java/leetcode/MaximumUniqueSubarrayTest.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximumUniqueSubarrayTest {
+
+    MaximumUniqueSubarray maximumUniqueSubarray;
+
+    @BeforeEach
+    public void setup(){
+        maximumUniqueSubarray = new MaximumUniqueSubarray();
+    }
+
+    @Test
+    public void should_returnSumOfUniqNum_when_arrayNotEmpty(){
+        int result = maximumUniqueSubarray.solution(new int[]{5,2,1,2,5,2,1,2,5});
+
+        assertEquals(8,result);
+    }
+}
