74b3e634132ddccdc938c3015435cf7a95a8878e
aligolgol <golgol.postbox@gmail.com>
2021-06-03 22:29:09 +0430

    TDD-Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts

---
+++ src/test/java/leetcode/MaxAreaTest.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxAreaTest {
+
+    MaxArea maxArea;
+
+    @BeforeEach
+    public void setup(){
+        maxArea = new MaxArea();
+    }
+
+    @Test
+    public void should_Return_the_maximum_area_of_a_piece_of_cake(){
+        int result = maxArea.accept(5,4,new int[]{3,1},new int[]{1});
+
+        assertEquals(6,result);
+    }
+
+}
