f3a919410e2ea3a31c58b91ed50ae09b3b1d41f0
aligolgol <golgol.postbox@gmail.com>
2021-06-08 20:42:31 +0430

    Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/main/java/leetcode/ConstructBinaryTreePreorderInorderTraversal.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class ConstructBinaryTreePreorderInorderTraversal {
+    public TreeNode buildTreeint(int[] preorder, int[] inorder){
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
