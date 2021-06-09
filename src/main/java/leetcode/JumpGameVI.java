ab9121ed1d3ca1819500f9e364f553d8f1c5e16e
aligolgol <golgol.postbox@gmail.com>
2021-06-09 22:03:57 +0430

    Jump Game VI-TDD

---
+++ src/main/java/leetcode/JumpGameVI.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class JumpGameVI {
+    public int maxResult(int[] nums, int k) {
+        return 0;
+    }
+}
---
+++ src/test/java/leetcode/JumpGameVITest.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JumpGameVITest {
+    JumpGameVI jumpGameVI;
+
+    @BeforeEach
+    public void setup(){
+        jumpGameVI = new JumpGameVI();
+    }
+
+    @Test
+    public void should_Return_the_maximum_score(){
+        int result = jumpGameVI.maxResult(new int[]{1,-1,-2,4,-7,3},2);
+
+        assertEquals(7,result);
+    }
+}
