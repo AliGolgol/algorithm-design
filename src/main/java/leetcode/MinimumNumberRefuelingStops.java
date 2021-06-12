4ccccdf75d4d3b2865f17ec32979ce4704b99ef5
aligolgol <golgol.postbox@gmail.com>
2021-06-12 21:21:23 +0430

    Minimum Number of Refueling Stops-Implemented

--- src/main/java/leetcode/MinimumNumberRefuelingStops.java
+++ src/main/java/leetcode/MinimumNumberRefuelingStops.java
@@ -2,6 +2,23 @@ package leetcode;
 
 public class MinimumNumberRefuelingStops {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        return 0;
+        int[] dp = new int[stations.length+1];
+        dp[0] = startFuel;
+
+        for(int i=0; i < stations.length; i++){
+            for(int j=i; j>=0; j--){
+                if(dp[j] >= stations[i][0]){
+                    dp[j+1] = Math.max(dp[j+1], dp[j]+stations[i][1]);
+                }
+            }
+        }
+
+        for(int i=0; i <= stations.length; i++){
+            if(dp[i] >= target){
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
