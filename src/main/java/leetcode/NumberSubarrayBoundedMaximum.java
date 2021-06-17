4ca8e6a73854aea0eee09fcb846c9b623bad4bba
aligolgol <golgol.postbox@gmail.com>
2021-06-17 22:14:45 +0430

    Number of Subarrays with Bounded Maximum-Implemented

--- src/main/java/leetcode/NumberSubarrayBoundedMaximum.java
+++ src/main/java/leetcode/NumberSubarrayBoundedMaximum.java
@@ -2,6 +2,13 @@ package leetcode;
 
 public class NumberSubarrayBoundedMaximum {
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
-        return 0;
+        int ans = 0, low = 0, mid = 0;
+        for (int num : nums) {
+            if (num > right) mid = 0;
+            else ans += ++mid;
+            if (num >= left) low = 0;
+            else ans -= ++low;
+        }
+        return ans;
     }
 }
