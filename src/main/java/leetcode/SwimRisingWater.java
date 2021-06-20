06ce70a585edbe451a8e8e26ddac3dd846fddf54
aligolgol <golgol.postbox@gmail.com>
2021-06-20 21:30:51 +0430

    Swim in Rising Water-TDD

---
+++ src/main/java/leetcode/SwimRisingWater.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class SwimRisingWater {
+    public int swimInWater(int[][] grid) {
+        return 0;
+    }
+}
---
+++ src/test/java/leetcode/SwimRisingWaterTest.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SwimRisingWaterTest {
+    SwimRisingWater swimRisingWater;
+
+    @BeforeEach
+    public void setup() {
+        swimRisingWater = new SwimRisingWater();
+    }
+
+    @Test
+    public void should_return_min_time_to_get_bottom_right(){
+        int result = swimRisingWater.swimInWater(new int[][]{{0, 2}, {1, 3}});
+
+        assertEquals(3,result);
+    }
+}
