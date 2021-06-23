b34c56595afa714cbcbb8622f3b68e21023d0792
aligolgol <golgol.postbox@gmail.com>
2021-06-23 18:12:35 +0430

    Reverse Linked List II-Implemented

--- src/main/java/leetcode/ReverseLinkedListII.java
+++ src/main/java/leetcode/ReverseLinkedListII.java
@@ -1,11 +1,38 @@
 package leetcode;
 
-public class ReverseLinkedListII {
-    public ListNode reverseBetween(ListNode head, int left, int right) {
+import java.util.Objects;
 
+public class ReverseLinkedListII {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode prv = dummy;
+        for(int i = 0; i < left - 1; i++){
+            prv = prv.next;
+        }
+
+        ListNode curr = prv.next;
+        for(int i = 0; i < right - left; i++){
+            ListNode next = curr.next;
+            curr.next = next.next;
+            next.next = prv.next;
+            prv.next = next;
+        }
+
+        return dummy.next;
+    }
+
+    public ReverseLinkedListII() {
+        super();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 
     static class ListNode {
+
         int val;
         ListNode next;
 
@@ -19,6 +46,19 @@ public class ReverseLinkedListII {
         ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ListNode listNode = (ListNode) o;
+            return val == listNode.val && Objects.equals(next, listNode.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, next);
         }
     }
 }
