4382066e5299e74aeec14da2b7f9a9e52c5d5df0
aligolgol <golgol.postbox@gmail.com>
2021-06-25 22:30:22 +0430

    Redundant Connection-TDD

---
+++ src/test/java/leetcode/RedundantConnectionTest.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RedundantConnectionTest {
+    RedundantConnection redundantConnection;
+
+    @BeforeEach
+    public void setup(){
+        redundantConnection = new RedundantConnection();
+    }
+
+    @Test
+    public void should_Return_an_edge(){
+        int[] result = redundantConnection.findRedundantConnection(new int[][]{{1,2},{2,3},{3,4},{1,4},{1,5}});
+
+        assertEquals(new int[]{1,4},result);
+    }
+}
