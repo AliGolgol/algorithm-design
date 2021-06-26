65fb5f735e392c5a4387a39ae00d912106f5e5ad
aligolgol <golgol.postbox@gmail.com>
2021-06-26 22:20:47 +0430

    Count of Smaller Numbers After Self-Implemented

--- src/test/java/leetcode/CountSmallerNumbersAfterSelfTest.java
+++ src/test/java/leetcode/CountSmallerNumbersAfterSelfTest.java
@@ -3,6 +3,10 @@ package leetcode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountSmallerNumbersAfterSelfTest {
@@ -15,9 +19,9 @@ class CountSmallerNumbersAfterSelfTest {
 
     @Test
     public void should_return_array_of_smaller_number_after_self(){
-        int[] result = countSmallerNumbersAfterSelf.countSmaller(new int[]{5,2,6,1});
+        List<Integer> result = countSmallerNumbersAfterSelf.countSmaller(new int[]{5,2,6,1});
         int[] expected = new int[]{2,1,1,0};
 
-        assertEquals(expected,result);
+        assertEquals(Arrays.stream(expected).boxed().collect(Collectors.toList()), result);
     }
 }
