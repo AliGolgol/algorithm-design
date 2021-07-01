2a490e435c2382cee0dedf3956f3c5b656656b83
aligolgol <golgol.postbox@gmail.com>
2021-07-01 20:35:44 +0430

    Gray Code-Implemented

--- src/test/java/leetcode/jul2021/GrayCode.java
+++ src/test/java/leetcode/jul2021/GrayCode.java
@@ -1,4 +1,17 @@
 package leetcode.jul2021;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GrayCode {
+    public List<Integer> accept(int n) {
+        List<Integer> result = new ArrayList<Integer>();
+        result.add(0);
+        for (int i = 0; i < n; i++) {
+            int size = result.size();
+            for (int k = size - 1; k >= 0; k--)
+                result.add(result.get(k) | 1 << i);
+        }
+        return result;
+    }
 }
