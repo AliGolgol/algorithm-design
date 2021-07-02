4a9d9a5b7894943ca14eb3b481b7ef3ac580ebfb
aligolgol <golgol.postbox@gmail.com>
2021-07-02 22:08:14 +0430

    Find K Closest Elements-Implemented

--- src/main/java/leetcode/jul2021/FindKClosestElements.java
+++ src/main/java/leetcode/jul2021/FindKClosestElements.java
@@ -1,4 +1,27 @@
 package leetcode.jul2021;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindKClosestElements {
+    public List<Integer> accept(int[] arr, int k, int x) {
+        int left = 0;
+        int right = arr.length - k;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = left; i < left + k; i++) {
+            result.add(arr[i]);
+        }
+
+        return result;
+    }
 }
