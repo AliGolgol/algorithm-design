4ec50a979bf820eecd5853f36e2fbc02a4477c5d
aligolgol <golgol.postbox@gmail.com>
2021-07-05 21:33:20 +0430

    Reshape the Matrix-TDD

---
+++ src/test/java/leetcode/jul2021/ReshapeMatrixTest.java
@@ -0,0 +1,28 @@
+package leetcode.jul2021;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReshapeMatrixTest {
+    ReshapeMatrix reshapeMatrix;
+
+    @BeforeEach
+    public void setup(){
+        reshapeMatrix = new ReshapeMatrix();
+    }
+
+    @Test
+    public void should_return_reshape_the_matrix(){
+        int[][] mat = {{1,2}, {3,4}};
+        int r = 1;
+        int c = 4;
+        int[][] expect = {{1,2,3,4}};
+        int[][] result = reshapeMatrix.accept(mat,r,c);
+
+        assertEquals(expect,result);
+
+    }
+
+}
