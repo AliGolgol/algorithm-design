aa433b274e4b03df592b29c66832a260108615c7
aligolgol <golgol.postbox@gmail.com>
2021-07-10 22:35:32 +0430

    Decode Ways II-Implemented

--- src/main/java/leetcode/jul2021/DecodeWaysII.java
+++ src/main/java/leetcode/jul2021/DecodeWaysII.java
@@ -1,4 +1,22 @@
 package leetcode.jul2021;
 
 public class DecodeWaysII {
+    public int numDecodings(String s) {
+        long mod = (long) Math.pow(10, 9) + 7;
+        long endingAny = 1, ending1 = 0, ending2 = 0;
+        for (char c : s.toCharArray()) {
+            long curEndingAny = 0;
+            if (c == '*') {
+                curEndingAny = 9 * endingAny + 9 * ending1 + 6 * ending2;
+                ending1 = endingAny;
+                ending2 = endingAny;
+            } else {
+                curEndingAny = (c == '0' ? 0 : endingAny) + ending1 + (c <= '6' ? ending2 : 0);
+                ending1 = (c == '1' ? endingAny : 0);
+                ending2 = (c == '2' ? endingAny : 0);
+            }
+            endingAny = curEndingAny % mod;
+        }
+        return (int) endingAny;
+    }
 }
