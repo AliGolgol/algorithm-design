c7171bf9eb240589c4e283114b2dba98cd81287d
aligolgol <golgol.postbox@gmail.com>
2021-07-13 23:38:00 +0430

    Find Peak Element-Implemented

--- src/main/java/leetcode/jul2021/FindPeakElement.java
+++ src/main/java/leetcode/jul2021/FindPeakElement.java
@@ -1,4 +1,15 @@
 package leetcode.jul2021;
 
 public class FindPeakElement {
+    public int accept(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid + 1])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
 }
