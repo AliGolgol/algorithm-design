3100a335cb98b04a30dc5e744fab2838d4baefd5
aligolgol <golgol.postbox@gmail.com>
2021-07-14 19:53:14 +0430

    Custom Sort String-Implemented

--- src/main/java/leetcode/jul2021/CustomSortString.java
+++ src/main/java/leetcode/jul2021/CustomSortString.java
@@ -1,4 +1,34 @@
 package leetcode.jul2021;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CustomSortString {
+    public String accept(String order, String str) {
+        Map<Character, Integer> strFrequencyMap = new HashMap<>();
+
+        for(char c: str.toCharArray()){
+            strFrequencyMap.put(c, strFrequencyMap.getOrDefault(c, 0) + 1);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(char c : order.toCharArray()){
+            if(!strFrequencyMap.containsKey(c)){
+                continue;
+            }
+
+            for(int i=0; i < strFrequencyMap.get(c); i++){
+                result.append(c);
+            }
+            strFrequencyMap.remove(c);
+        }
+
+        for(char c : strFrequencyMap.keySet()){
+            for(int i=0; i < strFrequencyMap.get(c); i++){
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
 }
