b281891651ed9c63eab1d11be916d0203936aed6
aligolgol <golgol.postbox@gmail.com>
2021-07-20 22:41:38 +0430

    Shuffle an Array-Implemented

--- src/main/java/leetcode/jul2021/ShuffleArray.java
+++ src/main/java/leetcode/jul2021/ShuffleArray.java
@@ -1,4 +1,41 @@
 package leetcode.jul2021;
 
+import java.util.Random;
+
 public class ShuffleArray {
+    private int[] array;
+    private int[] original;
+
+    Random rand = new Random();
+
+    public ShuffleArray(int[] nums) {
+        array = nums;
+        original = nums.clone();
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        array = original;
+        original = original.clone();
+        return original;
+    }
+
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        for (int i = 0; i < array.length; i++) {
+            swapAt(i, randRange(i, array.length));
+        }
+        return array;
+    }
+
+    private int randRange(int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private void swapAt(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
 }
