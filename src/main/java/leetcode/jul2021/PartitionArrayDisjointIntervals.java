52b71d02ff86a6de082f982ca0cf36f059c18507
aligolgol <golgol.postbox@gmail.com>
2021-07-22 22:51:55 +0430

    Partition Array into Disjoint Intervals-Implemented

--- src/main/java/leetcode/jul2021/PartitionArrayDisjointIntervals.java
+++ src/main/java/leetcode/jul2021/PartitionArrayDisjointIntervals.java
@@ -1,4 +1,19 @@
 package leetcode.jul2021;
 
 public class PartitionArrayDisjointIntervals {
+    public int partitionDisjoint(int[] nums) {
+        int leftMax = nums[0];
+        int max = nums[0];
+        int index = 0;
+
+        for(int i = 1; i < nums.length; i++){
+            max = Math.max(max, nums[i]);
+            if(nums[i] < leftMax){
+                leftMax = max;
+                index = i;
+            }
+        }
+
+        return index +1;
+    }
 }
