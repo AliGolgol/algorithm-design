f60a72bc6118088a401d404d32c6790785b12471
aligolgol <golgol.postbox@gmail.com>
2021-07-23 21:52:08 +0430

    Binary Tree Pruning-Implemented

--- src/main/java/leetcode/jul2021/BinaryTreePruning.java
+++ src/main/java/leetcode/jul2021/BinaryTreePruning.java
@@ -1,7 +1,23 @@
 package leetcode.jul2021;
 
-public class BinaryTreePruning {
+import java.util.Objects;
 
+public class BinaryTreePruning {
+    public TreeNode pruneTree(TreeNode root) {
+        return containsOne(root) ? root : null;
+    }
+
+    private boolean containsOne(TreeNode node) {
+        if (node == null) return false;
+
+        boolean leftContainsOne = containsOne(node.left);
+        boolean rightContainsOne = containsOne(node.right);
+
+        if (!leftContainsOne) node.left = null;
+        if (!rightContainsOne) node.right = null;
+
+        return node.val == 1 || leftContainsOne || rightContainsOne;
+    }
 
     public static class TreeNode {
         int val;
@@ -19,6 +35,19 @@ public class BinaryTreePruning {
             this.val = val;
             this.left = left;
             this.right = right;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            TreeNode treeNode = (TreeNode) o;
+            return val == treeNode.val && Objects.equals(left, treeNode.left) && Objects.equals(right, treeNode.right);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, left, right);
         }
     }
 }
