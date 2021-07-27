fe9b10780bf0f7da0fac54828bcd7be4b9b3d34d
aligolgol <golgol.postbox@gmail.com>
2021-07-27 22:34:36 +0430

    3Sum Closest-Implemented

--- src/test/java/leetcode/jul2021/ThreeSumClosestTest.java
+++ src/test/java/leetcode/jul2021/ThreeSumClosestTest.java
@@ -9,13 +9,13 @@ class ThreeSumClosestTest {
     ThreeSumClosest threeSumClosest;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         threeSumClosest = new ThreeSumClosest();
     }
 
     @Test
-    public void should_return_three_sum_closest(){
-        int result = threeSumClosest.accept(new int[]{-1,2,1,-4},1);
+    public void should_return_three_sum_closest() {
+        int result = threeSumClosest.accept(new int[]{-1, 2, 1, -4}, 1);
         assertEquals(2, result);
     }
 
